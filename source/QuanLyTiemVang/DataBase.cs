c617a74d004405b566f26bd66c6d0acf080cbe68
Thai Chu <thaick9x@gmail.com>
2015-07-01 22:51:51 +0700

    add function getMD5

--- source/QuanLyTiemVang/DataBase.cs
+++ source/QuanLyTiemVang/DataBase.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Data;
 using System.Data.OleDb;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace QuanLyTiemVang
 {
@@ -21,6 +22,7 @@ namespace QuanLyTiemVang
             {
                 connection = new OleDbConnection(_connectionString);
                 connection.Open();
+                
             }
             catch (OleDbException ex)
             {
@@ -56,5 +58,27 @@ namespace QuanLyTiemVang
             command.ExecuteNonQuery();
             CloseConnection();
         }
+
+        public static string getMD5(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //compute hash from the bytes of text
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+            //get hash result after compute it
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits
+                //for each byte
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+
     }
 }
